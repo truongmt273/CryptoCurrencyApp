7a27fac8509f483c6d6d25481403cbc2e5e08474
truongcse <truong.trinh2703@hcmut.edu.vn>
2022-02-24 21:02:18 +0700

    add remote data, model

---
+++ CryptoCurrency/app/src/main/java/com/truongtd/cryptocurrency/domain/model/CoinDetail.kt
@@ -0,0 +1,14 @@
+package com.truongtd.cryptocurrency.domain.model
+
+import com.truongtd.cryptocurrency.data.remote.dto.Team
+
+data class CoinDetail(
+    val coinId: String,
+    val name: String,
+    val description: String,
+    val symbol: String,
+    val rank: Int,
+    val isActive: Boolean,
+    val tags: List<String>,
+    val team: List<Team>
+)
